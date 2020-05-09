434390659b50530d696d959754bfdd1cca4f8293
STU17071 <matthias.studnicka@gmail.com>
2020-05-09 23:43:27 +0200

    Commit 05/09/2020Sat

--- Lüftersteuerung/sketch_A_Luefter/sketch_A_Luefter.ino
+++ Lüftersteuerung/sketch_A_Luefter/sketch_A_Luefter.ino
@@ -9,44 +9,13 @@ RTC_DS3231 rtc;
 /*char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};*/
 
 /*running Vent Parameters*/
-const unsigned long stunde = 3600; //min
-const unsigned long seconds = 1000; // 1min = 1000 millisekunden
+const unsigned long stunde = 3600000; // in seconds
+const unsigned long minutes = 3600; // in seconds
 int dauer = 2;
 int pause = 1;
-int beginn = 0;
 int ende = 0;
 int nextday = 0;
-DateTime current = new DateTime();
-/*Wochentage*/
 
-
-/*Sonntag*/
-int sonbegin = 6;
-int sonend = 22;
-
-/*Montag*/
-int mbegin = 6;
-int mend = 22;
-
-/*Dienstag*/
-int dibegin = 6;
-int diend = 22;
-
-/*Mittwoch*/
-int mitbegin = 6;
-int mitend = 22;
-
-/*Donnerstag*/
-int dobegin = 6;
-int doend = 22;
-
-/*Freitag*/
-int frbegin = 6;
-int frend = 22;
-
-/*Samstag*/
-int sambegin = 6;
-int samend = 22;
 
 void setup ()
 {
@@ -64,10 +33,9 @@ void setup ()
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
 
-  current = rtc.now();
-//  beginn = current.hour();
-//  ende = beginn + dauer;
-  nextday = current.day() + 1;
+
+  //ende = rtc.now().hour() + dauer;
+  nextday = rtc.now().day() + 1;
 }
 
 void TimeStringOutput(DateTime Uhrzeit)
@@ -79,171 +47,49 @@ void TimeStringOutput(DateTime Uhrzeit)
 
 void loop ()
 {
+  ende = rtc.now().hour() + pause;
   /*Runs for X Hours and make a break for Y Hours*/
-  current = rtc.now();
-  TimeStringOutput(current);
-  Serial.println((String)nextday);
-  if (beginn >= 22)
+
+  /*Wenn derzeitige Uhrzeit größer oder gleich 22 Uhr ist*/
+  if (rtc.now().hour() >= 22)
   {
+    if (ende < 0)
+    {
+      ende = ende + 24;
+    }
     //switchen
     ende = ende - 24;
     Serial.println(ende, DEC);
-    while (current.day() < nextday && current.hour() != 0)
+    while (rtc.now().day() < nextday && rtc.now().hour() != 0 && rtc.now().minute() != 0)
     {
       digitalWrite(air1, HIGH);
       digitalWrite(air2, HIGH);
       Serial.println("läuft ab 22 Uhr");
-      //Serial.println((String)beginn + " - " + (String)ende);
+      Serial.println((String)ende);
     }
     digitalWrite(air1, LOW);
     digitalWrite(air2, LOW);
-    Serial.println("Ventilatoren aus");
-    delay(100000);
-    Serial.println("hello");
-    beginn = current.hour();
-    ende = beginn + dauer;
-    nextday = current.day() + 1;
-    //delay(pause * stunde * seconds);
+    Serial.println("Ventilatoren aus 'in > 22 Uhr'");
+    delay(pause * stunde);
+    ende = rtc.now().hour() + dauer;
+    nextday = rtc.now().day() + 1;
   }
   else
   {
-    Serial.println("Gilt nur für den heutigen Tag");
+    Serial.println("Gilt nur für den heutigen Tag" + rtc.now().day());
     //  //Gilt nur für den heutigen Tag
-    while (current.hour() < ende)
+    while (rtc.now().hour() < ende)
     {
+      //TimeStringOutput(rtc.now());
       digitalWrite(air1, HIGH);
       digitalWrite(air2, HIGH);
-      Serial.println("läuft tagsüber bis 22 - " + (String)ende);
-      delay(100000);
-      //Serial.println((String)beginn + " - " + (String)ende);
     }
     digitalWrite(air1, LOW);
     digitalWrite(air2, LOW);
-    Serial.println("Ventilatoren sind heute " + (String)beginn + "aus");
-    delay(100000);
-    //delay(pause * stunde * seconds);
-    beginn = current.hour();
-    ende = beginn + dauer;
-    nextday = current.day() + 1;
+    // Serial.println("Ventilatoren sind heute " + (String)beginn + "aus");
+    Serial.println("läuft nicht");
+    delay(pause * stunde);
+    ende = rtc.now().hour() + dauer;
+    nextday = rtc.now().day() + 1;
   }
 }
-
-
-/*Methods for the Ventilators*/
-
-//void Wochentage()
-//{
-//DateTime now = rtc.now();
-//  switch (now.dayOfTheWeek())
-//  {
-//    case 0:
-//      digitalWrite(air1, Sunday(now));
-//      digitalWrite(air2, Sunday(now));
-//      break;
-//    case 1:
-//      digitalWrite(air1, Monday(now));
-//      digitalWrite(air2, Monday(now));
-//      break;
-//    case 2:
-//      digitalWrite(air1, Tuesday(now));
-//      digitalWrite(air2, Tuesday(now));
-//      break;
-//    case 3:
-//      digitalWrite(air1, Wednesday(now));
-//      digitalWrite(air2, Wednesday(now));
-//      break;
-//    case 4:
-//      digitalWrite(air1, Thursday(now));
-//      digitalWrite(air2, Thursday(now));
-//      break;
-//    case 5:
-//      digitalWrite(air1, Friday(now));
-//      digitalWrite(air2, Friday(now));
-//      break;
-//    case 6:
-//      digitalWrite(air1, Saturday(now));
-//      digitalWrite(air2, Saturday(now));
-//      break;
-//  }
-//}
-
-
-
-//boolean Sunday(DateTime now)
-//{
-//  while (now.hour() > sonbegin && now.hour() < sonend)
-//  {
-//    Serial.println("not run");
-//    return false;
-//  }
-//  Serial.println("run");
-//  return true;
-//}
-//
-//
-//boolean Monday(DateTime now)
-//{
-//  while (now.hour() > mbegin && now.hour() < mend)
-//  {
-//    Serial.println("not run");
-//    return false;
-//  }
-//  Serial.println("run");
-//  return true;
-//}
-//
-//boolean Tuesday(DateTime now)
-//{
-//  while (now.hour() > dibegin && now.hour() < diend)
-//  {
-//    Serial.println("not run");
-//    return false;
-//  }
-//  Serial.println("run");
-//  return true;
-//}
-//
-//boolean Wednesday(DateTime now)
-//{
-//  while (now.hour() > mitbegin && now.hour() < mitend)
-//  {
-//    Serial.println("not run");
-//    return false;
-//  }
-//  Serial.println("run");
-//  return true;
-//}
-//
-//
-//boolean Thursday(DateTime now)
-//{
-//  while (now.hour() > dobegin && now.hour() < doend)
-//  {
-//    Serial.println("not run");
-//    return false;
-//  }
-//  Serial.println("run");
-//  return true;
-//}
-//
-//boolean Friday(DateTime now)
-//{
-//  while (now.hour() > frbegin && now.hour() < frend)
-//  {
-//    Serial.println("not run");
-//    return false;
-//  }
-//  Serial.println("run");
-//  return true;
-//}
-//
-//boolean Saturday(DateTime now)
-//{
-//  while (now.hour() > sambegin && now.hour() < samend)
-//  {
-//    Serial.println("not run");
-//    return false;
-//  }
-//  Serial.println("run");
-//  return true;
-//}
